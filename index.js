e634f8653d4597195a60c99dd819e00fe50d8392
Christopher Marzulli <chriszulli15@gmail.com>
2022-04-25 12:57:43 -0400

    commit tree 689775f520b753a6d83cd6b803e484a7314b084f

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// test 1
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
 
-// Write your code here
+//test 2
+let random = Math.round(Math.random() + 1)
+
+//test 3
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+
+//test 4
+let max = Math.max(0, 5, 10, 20)
